ca76d7a301bc1ffa42f2c9bcacdf53962f0e1725
Aman Singh Negi <159263291+aman-singh-negi@users.noreply.github.com>
2025-05-14 12:55:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -8,22 +8,21 @@ using namespace std;
 
 class Solution {
   public:
+    bool check(vector<int>& arr, int target, int n, vector<vector<int>>& dp)
+    {
+        if(target==0)return true;
+        if(n==0)return false;
+        if(dp[n][target]!=-1)return dp[n][target];
+        if(arr[n-1]>target)return dp[n][target]=check(arr,target,n-1,dp);
+        else return dp[n][target]=check(arr,target,n-1,dp) || check(arr,target-arr[n-1],n-1,dp);
+    }
     bool isSubsetSum(vector<int>& arr, int sum) {
         // code here
-        int n= arr.size();
-        int dp[n+1][sum+1];
-        memset(dp,0,sizeof(dp));
-        for(int i=0;i<sum+1;i++)dp[0][i]=0;
-        for(int i=0;i<n+1;i++)dp[i][0]=1;
-        for(int i=1;i<n+1;i++)
-        {
-            for(int j=1;j<sum+1;j++)
-            {
-                if(arr[i-1]<=j)dp[i][j]=dp[i-1][j-arr[i-1]] || dp[i-1][j];
-                else dp[i][j]=dp[i-1][j];
-            }
-        }
-        return dp[n][sum];
+        int n=arr.size();
+        vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
+        for(int i=0;i<=sum;i++)dp[0][i]=0;
+        for(int i=0;i<=n;i++)dp[i][0]=1;
+        return check(arr,sum,n,dp);
     }
 };
 
