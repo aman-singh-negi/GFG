8560f2daa9a9fe5fbb5fdb24ac98067da5681d98
Aman Singh Negi <159263291+aman-singh-negi@users.noreply.github.com>
2025-07-03 00:43:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.cpp
+++ Difficulty: Medium/Frog Jump/frog-jump.cpp
@@ -1,53 +1,18 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
-    private:
-        int find(vector<int>&height,int idx,vector<int>&dp)
-        {
-            if(idx==0)return 0;
-            if(idx==1)return abs(height[1]-height[0]);
-            if(dp[idx]!=-1)return dp[idx];
-            
-            
-            int oneStep=abs(height[idx]-height[idx-1])+find(height,idx-1,dp);
-            int twoStep=abs(height[idx]-height[idx-2])+find(height,idx-2,dp);
-            return dp[idx]=min(oneStep,twoStep);
-        }
   public:
     int minCost(vector<int>& height) {
         // Code here
         int n=height.size();
-        vector<int>dp(n+1,-1);
-        return find(height,n-1,dp);
+        int dp[n];
+        memset(dp,-1,sizeof(dp));
+
+        if(n<=1)return 0;
+        dp[0]=0;
+        dp[1]=abs(height[1]-height[0]);
+        for(int i=2;i<n;i++)
+        {
+            dp[i]=min(dp[i-1]+abs(height[i]-height[i-1]),dp[i-2]+abs(height[i]-height[i-2]));
+        }
+        return dp[n-1];
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    string str;
-    getline(cin, str);
-    int t = stoi(str);
-    while (t--) {
-        getline(cin, str);
-        stringstream ss(str);
-        vector<int> arr;
-        int num;
-        while (ss >> num) {
-            arr.push_back(num);
-        }
-        Solution ob;
-        cout << ob.minCost(arr) << endl;
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
