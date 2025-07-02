d0b8ae03e944fe44013f94e950fc5f8601d0a845
Aman Singh Negi <159263291+aman-singh-negi@users.noreply.github.com>
2025-07-03 00:48:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.cpp
+++ Difficulty: Medium/Frog Jump/frog-jump.cpp
@@ -3,16 +3,16 @@ class Solution {
     int minCost(vector<int>& height) {
         // Code here
         int n=height.size();
-        int dp[n];
-        memset(dp,-1,sizeof(dp));
 
         if(n<=1)return 0;
-        dp[0]=0;
-        dp[1]=abs(height[1]-height[0]);
+        int dp0=0;
+        int dp1=abs(height[1]-height[0]);
         for(int i=2;i<n;i++)
         {
-            dp[i]=min(dp[i-1]+abs(height[i]-height[i-1]),dp[i-2]+abs(height[i]-height[i-2]));
+            int dp2=min(dp1+abs(height[i]-height[i-1]),dp0+abs(height[i]-height[i-2]));
+            dp0=dp1;
+            dp1=dp2;
         }
-        return dp[n-1];
+        return dp1;
     }
 };
